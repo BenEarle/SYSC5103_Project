069b725b5bcaa4ae08a8b9653b7e98efda878bcd
Ben Earle <ben.earle@live.ca>
2020-03-21 15:49:46 -0400

    Adding Krislet and an empty project to get started.

---
+++ KrisletEnvironment.java
@@ -0,0 +1,33 @@
+// Environment code for project SYSC5103_Project.mas2j
+
+import jason.asSyntax.*;
+import jason.environment.*;
+import java.util.logging.*;
+
+public class KrisletEnvironment extends Environment {
+
+    private Logger logger = Logger.getLogger("SYSC5103_Project.mas2j."+KrisletEnvironment.class.getName());
+
+    /** Called before the MAS execution with the args informed in .mas2j */
+    @Override
+    public void init(String[] args) {
+        super.init(args);
+        addPercept(ASSyntax.parseLiteral("percept(demo)"));
+    }
+
+    @Override
+    public boolean executeAction(String agName, Structure action) {
+        logger.info("executing: "+action+", but not implemented!");
+        if (true) { // you may improve this condition
+             informAgsEnvironmentChanged();
+        }
+        return true; // the action was executed with success
+    }
+
+    /** Called before the end of MAS execution */
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
+
